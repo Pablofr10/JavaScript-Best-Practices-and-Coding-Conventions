6efd318dde5a70fedbf70a243cfef20ca3375424
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-22 11:46:08 -0300

    Magic Numbers

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Magic Numbers
 
+// BEFORE
+for (let i = 0; i < 86400; i++) {
+  // ...
+}
+
+//AFTER
 const SECOUNDS_IN_A_DAY = 86400;
 for (let i = 0; i < SECOUNDS_IN_A_DAY; i++) {
   // ...
 }
+
+
+// Deep nesting 
+const 
