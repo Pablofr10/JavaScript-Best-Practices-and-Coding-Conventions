0a0dd753be10b29f791ab5d55b703458b27daed6
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-22 12:09:38 -0300

    Avoid large functions

--- index.js
+++ index.js
@@ -36,3 +36,23 @@ const retriveFinalValue = (element) => {
 retriveFinalValue(exampleArray);
 
 // Stop writing comments - Code must speak for itself!
+
+// Avoid large functions
+
+// BEFORE
+const addMultiplySubstract = (a, b, c) => {
+  // addition
+  const addition = a + b + c;
+  // multiplication
+  const multiplication = a * b * c;
+  // subraction
+  const subraction = a - b - c;
+
+  //return a string (addition, multiplication, subraction)
+  return `${addition} ${multiplication} ${subraction}`;
+};
+
+// AFTER
+const add = (a, b, c) => a + b + c;
+const multiply = (a, b, c) => a * b * c;
+const subtract = (a, b, c) => a - b - c;
